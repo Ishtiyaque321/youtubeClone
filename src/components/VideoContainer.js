674bfd79babb7eecb6c2fbe59508a39876868791
Ishtiyaque Ahmad <istiyaqueahmad0@gmail.com>
2024-02-24 21:49:35 +0530

    project is ready

---
+++ src/components/VideoContainer.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { YOUTUBE_VIDEOS_API } from "../utils/contants";
+import VideoCard, { AdVideoCard } from "./VideoCard";
+import { Link } from "react-router-dom";
+
+const VideoContainer = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const getVideos = async () => {
+      try{
+      const data = await fetch(YOUTUBE_VIDEOS_API);
+      const json = await data.json();
+      setVideos(json?.items);
+      }catch(error){
+        console.log("error");
+      }
+    };
+  
+    getVideos();
+  }, []);
+
+
+  return (
+    <div className="flex flex-wrap">
+      {videos[0] && <AdVideoCard info={videos[0]} />}
+      {videos.map((video) => (
+        <Link key={video.id} to={"/watch?v=" + video.id}>
+          <VideoCard info={video} />
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default VideoContainer;
